f182b098bc0946d8f138caf98b67a2a4f3676211
Rungkarn <luuca@hotmail.com>
2011-07-30 11:09:53 +0700

    boring

--- src/com/jk/testgit/mainjkTestGit.java
+++ src/com/jk/testgit/mainjkTestGit.java
@@ -9,5 +9,6 @@ public class mainjkTestGit extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        //karn
     }
 }
